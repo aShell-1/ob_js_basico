4a5af0801f2924f57d8aa1e596b66deb3960d7ea
aShell <109769755+aShell-1@users.noreply.github.com>
2022-10-15 16:19:21 -0500

    Add files via upload

---
+++ 02-syntax-variables-reservadas/ejercicio.js
@@ -0,0 +1,10 @@
+let bookdetails = {
+    title: 'It',
+    autor: 'Stephen King',
+    year: 1986,
+    url: 'https://www.amazon.com/Stephen-King/dp/0670813028'
+}
+// nombre , edad, esDesarrollador, fechaNacimiento, favBookObject
+let mydata = ['axell', 25, true, new Date(1997, 09 - 1, 08), bookdetails]
+
+console.log(mydata)
